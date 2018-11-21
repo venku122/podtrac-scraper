4f64c37a4c1c50ffeb1d21f72813fe9d064ef8ef
T.J. Tarazevits <tarazevits@gmail.com>
2018-11-20 22:38:51 -0700

    Improved page parsing
    Properly wait for page loads.
    
    Parse data rows into storable format.

--- index.js
+++ index.js
@@ -12,15 +12,20 @@ const loginToPodtrac = async () => {
   console.log('puppeteer loaded login page');
   await page.type('#Email', email);
   await page.type('#ClearPasscode', password);
-
-  await page.click('.button');
   console.log('form button clicked');
-  await page.waitForNavigation();
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
+    await page.click('.button'),
+  ]);
+
   console.log(`New Page URL: ${page.url()}`);
   const dashboardURl = page.url();
 
   console.log('clicking on episode playcount');
-  await page.$$eval('.stats-cell a', links => links[1].click());
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
+    page.$$eval('.stats-cell a', links => links[1].click()),
+  ]);
   console.log(`New Page URL: ${page.url()}`);
 
  const tableHeaders = await page.evaluate(() => {
@@ -47,8 +52,57 @@ const tableData = await page.evaluate(() => {
   });
 });
 
+const tableDataRows = await page.evaluate(() => {
+  const rows = Array.from(document.querySelectorAll('.data-row'));
+  const parsedRows = [];
+  rows.map(row => {
+    //console.dir(row);
+    const parsedRow = {};
+    let periodTotal = 0;
+    for (let i = 0; i < row.children.length; i++) {
+      const child = row.children[i];
+      if (child.tagName === 'TH') {
+        let episodeName = child.innerText.replace('\t', '').replace('\n', '');
+        parsedRow.episode = episodeName;
+      }
+      if (child.tagName === 'TD') {
+        if (child.className === 'total-cell') {
+          let total = child.innerText.replace('\t', '').replace('\n', '').replace(/,/g, '');
+          let numericValue = 0;
+          if (total !== '-') {
+            numericValue = Number.parseInt(total, 10);
+          }
+          parsedRow.allTimeTotal = numericValue;
+        } else {
+          let weeklyDownloads = child.innerText.replace('\t', '').replace('\n', '').replace(/,/g, '');
+          let numericValue = 0;
+          if (weeklyDownloads !== '-') {
+            numericValue = Number.parseInt(weeklyDownloads, 10);
+          }
+          periodTotal += numericValue;
+          parsedRow[i.toString()] = numericValue;
+        }
+      }
+    }
+    parsedRow.periodTotal = periodTotal;
+    parsedRows.push(parsedRow);
+     // const txt = row.innerText.replace('\t', '').replace('\n', '').replace(',', '');
+     // return txt.replace(/<a [^>]+>[^<]*<\/a>/g, '').trim();
+     /*
+     let numericValue = 0;
+     if (txt !== '-') {
+       numericValue = Number.parseInt(txt, 10);
+     }
+     console.log(`table row data: ${numericValue}`);
+     return numericValue;
+     */
+  });
+  return parsedRows;
+});
+
 console.dir(tableHeaders);
 console.dir(tableData);
+console.dir(tableDataRows);
 }
 
 loginToPodtrac();
